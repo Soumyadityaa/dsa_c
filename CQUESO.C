e68e28a04ac42a10450ff1f559f75f9289955524
soumyaditya bauri <116110445+Soumyadityaa@users.noreply.github.com>
2023-08-04 16:18:33 +0530

    Add files via upload

---
+++ CQUESO.C
@@ -0,0 +1,123 @@
+#include<stdio.h>
+#include<conio.h>
+#define size 10
+int queue[size],front=-1,rear=-1;
+void addq()
+{
+ int element;
+
+ if((front==0&&rear==size-1)||(front==rear+1))
+ {
+   printf("queue is full\n");
+ }
+
+ else
+ {
+   if(rear!=(size-1))
+   {
+    rear=rear+1;
+    if(front==-1)
+    {
+      front=front+1;
+    }
+
+    printf("enter value:");
+    scanf("%d",&element);
+    queue[rear]=element;
+   }
+
+   if((rear=(size-1))&&(front!=0))
+   {
+	rear=0;
+	printf("enter value:");
+	scanf("%d",&element);
+	queue[rear]=element;
+   }
+ }
+
+}
+
+void delq()
+{
+  int temp;
+  if(front==-1){
+  printf("queue is empty");
+  }
+
+  else
+  {
+    temp=queue[front];
+
+    if(front==rear)
+    {
+     front=rear=-1;
+    }
+
+    else
+    {
+      if(front==size-1)
+      {
+       front=0;
+      }
+
+      else
+      {
+       front=front+1;
+      }
+    }
+
+    printf("%d is deleted sucessfully...:)\n",temp);
+
+  }
+
+}
+void display()
+{
+  int i;
+  if(front==-1){
+   printf("queue is empty, So nothing is display \n");
+  }
+  else
+  {
+    for(i=front;i<=rear;i++)
+    {
+     printf("%d ",queue[i]);
+    }
+  }
+
+}
+void main()
+{
+ int ch;
+ clrscr();
+
+
+
+ do
+ {
+  printf("\nenter choice...\n1.addq\n2.delq\n3.display queue\n4.exit\n");
+  scanf("%d",&ch);
+  switch(ch)
+  {
+   case 1:
+   addq();
+   break;
+
+   case 2:
+   delq();
+   break;
+
+   case 3:
+   display();
+   break;
+
+   default:
+   printf("invalid choice and you exit");
+   break;
+  }
+
+ } while(ch!=4);
+
+ getch();
+
+}
