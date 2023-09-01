9be9e039151afa199025471f884d66a9fec9fa97
soumyaditya bauri <116110445+Soumyadityaa@users.noreply.github.com>
2023-09-01 15:03:11 +0530

    Create Quick_sort.c

---
+++ Quick_sort.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+// Function to swap two elements
+void swap(int arr[], int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+// Quick Sort function
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j <= high - 1; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+
+        quickSort(arr, low, i);
+        quickSort(arr, i + 2, high);
+    }
+}
+
+int main() {
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    quickSort(arr, 0, n - 1);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
